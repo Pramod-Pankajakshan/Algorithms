c4ec161622864936e33af6504ec85544fd35aff0
Pramod-Pankajakshan <pramodp3@gmail.com>
2021-01-21 15:00:22 -0500

    added qn

--- basic/RussianDoll.java
+++ basic/RussianDoll.java
@@ -4,7 +4,22 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+/**
+You have a number of envelopes with widths and heights given as a pair of integers (w, h). 
+One envelope can fit into another if and only if both the width and height of one envelope is 
+greater than the width and height of the other envelope.
+What is the maximum number of envelopes can you Russian doll? (put one inside other)
 
+Note:
+Rotation is not allowed.
+
+Example:
+
+Input: [[5,4],[6,4],[6,7],[2,3]]
+Output: 3 
+Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+
+**/
 public class RussianDoll {
    public static void main(String[] args) {
 	int [][]j = {{5,4},{6,4},{6,7},{2,3}};
