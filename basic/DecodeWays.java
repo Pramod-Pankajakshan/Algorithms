bf48ab92dc451f297540f234a0ad8fe3dc5776cc
Pramod-Pankajakshan <pramodp3@gmail.com>
2021-02-15 13:25:19 -0500

    Create DecodeWays.java

---
+++ basic/DecodeWays.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.Arrays;
+
+public class DecodeWays {
+	public static void main(String[] args) {		
+		System.out.println(numDecodings(new String("226111")));
+	}
+
+	
+	static int cnt = 0;
+
+	  public static int numDecodings(String s) {
+
+	       
+
+	    }
+	
+	
+
+}
